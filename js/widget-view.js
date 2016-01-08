225a7f18640b69d890d42cab20752e264a357803
Weston Ruter <weston@xwp.co>
2016-01-08 00:20:43 -0800

    Use collection.hasMore()

--- js/widget-view.js
+++ js/widget-view.js
@@ -38,16 +38,12 @@ var nextRecentPostsWidget = (function( $ ) {
 		// @todo Try http://stackoverflow.com/a/20419831
 
 		initialize: function() {
-			var view = this, data, posts, watchAuthorChanges;
+			var view = this, data, watchAuthorChanges;
 
 			data = JSON.parse( $( view.el ).find( '> script[type="application/json"]' ).text() );
 			view.model = new self.WidgetModel( data.instance );
 			view.args = data.args;
 			view.collection = new wp.api.collections.Posts( data.posts, { parse: true } );
-			if ( ! data.posts ) {
-				view.collection.fetch();
-			}
-
 			view.template = wp.template( 'next-recent-posts-widget' );
 
 			view.collection.fetch = function( options ) {
@@ -71,14 +67,15 @@ var nextRecentPostsWidget = (function( $ ) {
 
 			view.collection.on( 'change', function() {
 				view.render();
+				// @todo re-sort according to the most options data
 			} );
 			view.model.on( 'change', function() {
 				view.render();
 			} );
 			view.collection.on( 'add', watchAuthorChanges );
 			view.collection.each( watchAuthorChanges );
-			view.collection.on( 'sync', function( collection, response, options ) {
-				view.model.set( 'has_more', view.model.get( 'number' ) < options.xhr.getResponseHeader( 'X-WP-Total' ) );
+			view.collection.on( 'sync', function( collection ) {
+				view.model.set( 'has_more', collection.hasMore() );
 				view.render();
 			} );
 
@@ -87,6 +84,10 @@ var nextRecentPostsWidget = (function( $ ) {
 			} );
 
 			// @todo If we're in the Customizer preview, make sure that this.model gets updated whenever the widget setting gets updated.
+
+			if ( ! data.posts ) {
+				view.collection.fetch();
+			}
 
 			view.render();
 			view.render = _.debounce( view.render );
